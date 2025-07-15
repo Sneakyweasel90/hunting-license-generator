650ba938894ecbcc4b4e7355d241fd3eb8dc2ea9
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-07-15 20:39:32 -0300

    small change

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="main">
       <h1 className="title">Random Hunting License Generator</h1>
-      <LicenseForm />
       <Analytics />
+      <LicenseForm />
     </div>
   );
 }
