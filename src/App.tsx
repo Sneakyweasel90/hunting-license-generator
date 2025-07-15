f97d6881c2002ecdf3f33136f1cc21bd849f4629
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-07-15 20:36:47 -0300

    tracking added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import LicenseForm from "./components/LicenseForm";
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="main">
       <h1 className="title">Random Hunting License Generator</h1>
       <LicenseForm />
+      <Analytics />
     </div>
   );
 }
