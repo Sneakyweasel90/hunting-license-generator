fb9daa35c249a9d74d46e1eda9894ecbbcb59cfe
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-07-15 21:38:54 -0300

    added background images

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <div className="main">
-      <h1 className="title">Random Hunting License Generator</h1>
+      <h1 className="title">theHunter: Call of the Wild Tag Generator</h1>
       <Analytics />
       <LicenseForm />
     </div>
