bc147f71b478cbbe03be0807ce46f7d04a2d6d39
Sneakyweasel90 <134094143+Sneakyweasel90@users.noreply.github.com>
2025-07-15 21:44:23 -0300

    removed from app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import LicenseForm from "./components/LicenseForm";
 import "./App.css";
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="main">
       <h1 className="title">theHunter: Call of the Wild Tag Generator</h1>
-      <Analytics />
       <LicenseForm />
     </div>
   );
